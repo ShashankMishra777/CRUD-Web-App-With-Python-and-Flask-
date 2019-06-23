47dc2890d1a354464ea1b9eae60bbb83b99b7459
ShashankMishra777 <50677721+Noobcoder777@users.noreply.github.com>
2019-06-23 20:39:31 +0530

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -6,7 +6,7 @@ mysql = MySQL()
  
 # MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'Mishra@123'
+app.config['MYSQL_DATABASE_PASSWORD'] = '**********'
 app.config['MYSQL_DATABASE_DB'] = 'mydatabase11'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
